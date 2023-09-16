019eb4797c0d19cb776be684fe3eaa4a2461986e
Subhashri R <70220661+subha-2001@users.noreply.github.com>
2023-09-16 12:59:46 +0530

    Add files via upload

---
+++ Task 1/script.js
@@ -0,0 +1,24 @@
+function validString() {
+    const inputString=document.getElementById("inputString").value;
+    const result=document.getElementById("result");
+    if (!inputString.trim()) {
+        result.textContent = "Please provide any string to validate!";
+        result.style.color="red";
+        return;
+    }
+    if(BracesValid(inputString)) {
+        result.textContent="Valid";
+        result.style.color="turquoise";
+    
+    }
+    else {
+        result.textContent="Invalid";
+        result.style.color="red";
+    }
+}
+function BracesValid(input) {
+    const regex=/\(\)|\{\}|\[\]/g;
+    let lastInput=input;
+    while(lastInput!=(lastInput=lastInput.replace(regex,''))){ }
+    return lastInput.length===0;
+}
