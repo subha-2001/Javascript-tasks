51f5c31b6ca9fc9792ad5c54d3e7706020ac8af9
Subhashri R <70220661+subha-2001@users.noreply.github.com>
2023-09-17 08:42:50 +0530

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,24 +1,32 @@
 function validString() {
-    const inputString=document.getElementById("inputString").value;
-    const result=document.getElementById("result");
-    if (!inputString.trim()) {
-        result.textContent = "Please provide any string to validate!";
-        result.style.color="red";
+    const inputString = document.getElementById("inputString").value;
+    const result = document.getElementById("result");
+
+    // Check if the input contains only parentheses, brackets, and braces
+    if (!/^[(){}\[\]\s]*$/.test(inputString)) {
+        result.textContent = "Please enter only parentheses, brackets, and braces!";
+        result.style.color = "red";
         return;
     }
+
+    // If the input is empty, show an error message
+    if (!inputString.trim()) {
+        result.textContent = "Please provide any parentheses, brackets, or braces to validate!";
+        result.style.color = "red";
+        return;
+    }
+
     const inputWithoutSpaces = inputString.replace(/\s/g, "");
 
     if (BracesValid(inputWithoutSpaces)) {
-
-        result.textContent="Valid";
-        result.style.color="turquoise";
-    
-    }
-    else {
-        result.textContent="Invalid";
-        result.style.color="red";
+        result.textContent = "Valid String";
+        result.style.color = "turquoise";
+    } else {
+        result.textContent = "Invalid String";
+        result.style.color = "red";
     }
 }
+// Checks if a string with parentheses, brackets, and braces is valid.
 function BracesValid(input) {
     const regex = /\(\)|\{\}|\[\]/g;
     let lastInput = input;
