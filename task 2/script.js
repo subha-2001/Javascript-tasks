0c1a458dd7f79884cbb0e57e506d9542c9f1bd83
Subhashri R <70220661+subha-2001@users.noreply.github.com>
2023-09-20 10:32:36 +0530

    Update script.js

--- task 2/script.js
+++ task 2/script.js
@@ -1,30 +1,45 @@
-
+/**
+ * Validates and retrieves location information for a US zipcode.
+ */
 function validZipcode() {
-    const zipcode=document.getElementById("zipcode").value;
-    if(!zipcode.trim()) {
-        result.textContent="Please provide any US Zipcode!";
-        result.style.color="red";
+    const zipcode = document.getElementById("zipcode").value;
+    const result = document.getElementById("result");
+
+    if (!zipcode.trim()) {
+        result.textContent = "Please provide any US Zipcode!";
+        result.style.color = "red";
         return;
     }
-    if((/^\d{5}$/.test(zipcode))) {
-    fetch(`https://ziptasticapi.com/${zipcode}`) 
-    .then(response=>response.json())
-    .then(data=> {
-        document.getElementById("country").textContent=`Country:${data.country}`;
-        document.getElementById("city").textContent=`City:${data.city}`;
-        document.getElementById("state").textContent=`State:${data.state}`;
-    const mapDiv=document.getElementById("map");
-    mapDiv.innerHTML=`<a href="https://www.google.com/maps/place/${data.country},${data.state},${zipcode}" target="_blank">View on Google Maps</a>`;
-    })
-    .catch(error => console.error(error));
-}
-else {
-    document.getElementById("result").textContent="The given zipcode is invalid";
 
-    result.style.color="red";}
+    if (/^\d{5}$/.test(zipcode)) {
+        fetch(`https://ziptasticapi.com/${zipcode}`)
+            .then(response => response.json())
+            .then(data => {
+                const { country, city, state } = data;
+                result.innerHTML = `
+                    <p>Country: ${country}</p>
+                    <p>State: ${state}</p>
+                    <p>City: ${city}</p>
+                    <div id="map">
+                        <a href="https://www.google.com/maps/place/${country},${state},${zipcode}" target="_blank">View on Google Maps</a>
+                    </div>
+                `;
+                result.style.color='grey';
+            })
+            .catch(error => console.error(error));
+    } else {
+        result.textContent = "The given zipcode is invalid. Please provide a valid 5-digit US zipcode.";
+        result.style.color = "red";
+    }
 }
+
+/**
+ * Clears the result content and enables the submit button.
+ */
 document.querySelector('#resetbtn').addEventListener('click', function () {
-    // Clear result and error messages
-    document.getElementById("result").style.display = "none";
-    document.getElementById("error-message").style.display = "none";
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("submitbtn").disabled = false;
 });
+
+// Attach the validZipcode function to the submit button click event.
+document.querySelector('#submitbtn').addEventListener('click', validZipcode);
